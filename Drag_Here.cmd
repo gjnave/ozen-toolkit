d76d784f66389ccf845118de1dc47124a18b3962
Gary James Nave (Cognbuild) <59668562+gjnave@users.noreply.github.com>
2024-09-12 14:01:47 -0600

    Update Drag_Here.cmd
    Fixed Miniconda search. Simply change line 28 to your installation directory

--- Drag_Here.cmd
+++ Drag_Here.cmd
@@ -1,4 +1,6 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :: This file is part of sygil-webui (https://github.com/Sygil-Dev/sygil-webui/).
 :: 
 :: Copyright 2022 Sygil-Dev team.
@@ -14,57 +16,48 @@
 :: 
 :: You should have received a copy of the GNU Affero General Public License
 :: along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
 :: Run all commands using this script's directory as the working directory
 cd %~dp0
 
-:: copy over the first line from environment.yaml, e.g. name: ldm, and take the second word after splitting by ":" delimiter
-set v_conda_env_name="ozen"
+:: Set the conda environment name
+set v_conda_env_name=ozen
+echo Environment name is set as %v_conda_env_name%
 
+:: ***Set the path to your Miniconda3 installation
+set v_conda_path=C:\miniconda3
 
-echo Environment name is set as %v_conda_env_name% as per environment.yaml
-
-:: Put the path to conda directory in a file called "custom-conda-path.txt" if it's installed at non-standard path
-IF EXIST custom-conda-path.txt (
-  FOR /F %%i IN (custom-conda-path.txt) DO set v_custom_path=%%i
+:: Check if the Miniconda3 directory exists
+if not exist "%v_conda_path%" (
+    echo Miniconda3 not found at %v_conda_path%
+    echo Please ensure Miniconda3 is installed at this location or modify the script with the correct path.
+    pause
+    exit /b 1
 )
 
-set INSTALL_ENV_DIR=%cd%\installer_files\env
-set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%INSTALL_ENV_DIR%\Library\usr\bin;%PATH%
+echo Found Miniconda3 at %v_conda_path%
 
-set v_paths=%INSTALL_ENV_DIR%
-set v_paths=%v_paths%;%ProgramData%\miniconda3
-set v_paths=%v_paths%;%USERPROFILE%\miniconda3
-set v_paths=%v_paths%;%ProgramData%\anaconda3
-set v_paths=%v_paths%;%USERPROFILE%\anaconda3
-
-for %%a in (%v_paths%) do (
-  IF NOT "%v_custom_path%"=="" (
-    set v_paths=%v_custom_path%;%v_paths%
-  )
-)
-
-for %%a in (%v_paths%) do (
-  if EXIST "%%a\Scripts\activate.bat" (
-    SET v_conda_path=%%a
-    echo anaconda3/miniconda3 detected in %%a
-    goto :CONDA_FOUND
-  )
-)
-
-IF "%v_conda_path%"=="" (
-  echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
-  pause
-  exit /b 1
-)
-
-:CONDA_FOUND
-echo Starting conda environment %v_conda_env_name% from %v_conda_path%
-
+:: Activate the conda environment
+echo Activating conda environment: %v_conda_env_name%
 call "%v_conda_path%\Scripts\activate.bat" "%v_conda_env_name%"
+if %errorlevel% neq 0 (
+    echo Failed to activate Conda environment. Error level: %errorlevel%
+    echo Make sure the environment "%v_conda_env_name%" exists.
+    echo You may need to create it first using the setup script.
+    pause
+    exit /b 1
+)
 
-::python windows_install.py
+echo Conda environment activated successfully
+
 :START_GUI
-::set HasChanges=0
+echo Starting Ozen GUI...
 python ozen.py "%~1"
+if %errorlevel% neq 0 (
+    echo Failed to start Ozen GUI. Error level: %errorlevel%
+    pause
+    exit /b 1
+)
+
+echo Ozen GUI has closed. Press any key to exit.
 pause
-:cmd /k
