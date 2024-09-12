325782ee95e9154fcf55b6eb75514d3d528afd30
Gary James Nave (Cognbuild) <59668562+gjnave@users.noreply.github.com>
2024-09-12 14:02:42 -0600

    Update Set Up Ozen.bat
    Fixed finding Anaconda Installation problem.
    Simply change line 18 to your installation directory

--- Set Up Ozen.bat
+++ Set Up Ozen.bat
@@ -1,77 +1,62 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :: This file is part of sygil-webui (https://github.com/Sygil-Dev/sygil-webui/).
-:: 
-:: Copyright 2022 Sygil-Dev team.
-:: This program is free software: you can redistribute it and/or modify
-:: it under the terms of the GNU Affero General Public License as published by
-:: the Free Software Foundation, either version 3 of the License, or
-:: (at your option) any later version.
-:: 
-:: This program is distributed in the hope that it will be useful,
-:: but WITHOUT ANY WARRANTY; without even the implied warranty of
-:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-:: GNU Affero General Public License for more details.
-:: 
-:: You should have received a copy of the GNU Affero General Public License
-:: along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
 :: Run all commands using this script's directory as the working directory
 cd %~dp0
 
-:: copy over the first line from environment.yaml, e.g. name: ldm, and take the second word after splitting by ":" delimiter
+:: Copy over the first line from environment.yaml, e.g. name: ldm, and take the second word after splitting by ":" delimiter
 for /F "tokens=2 delims=: " %%i in (environment.yaml) DO (
   set v_conda_env_name=%%i
   goto EOL
 )
 :EOL
-
 echo Environment name is set as %v_conda_env_name% as per environment.yaml
 
-:: Put the path to conda directory in a file called "custom-conda-path.txt" if it's installed at non-standard path
-IF EXIST custom-conda-path.txt (
-  FOR /F %%i IN (custom-conda-path.txt) DO set v_custom_path=%%i
+:: ***Set the path to your Miniconda3 installation
+set v_conda_path=C:\miniconda3
+
+:: Check if the Miniconda3 directory exists
+if not exist "%v_conda_path%" (
+    echo Miniconda3 not found at %v_conda_path%
+    echo Please ensure Miniconda3 is installed at this location or modify the script with the correct path.
+    pause
+    exit /b 1
 )
 
-set INSTALL_ENV_DIR=%cd%\installer_files\env
-set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%INSTALL_ENV_DIR%\Library\usr\bin;%PATH%
+echo Found Miniconda3 at %v_conda_path%
 
-set v_paths=%INSTALL_ENV_DIR%
-set v_paths=%v_paths%;%ProgramData%\miniconda3
-set v_paths=%v_paths%;%USERPROFILE%\miniconda3
-set v_paths=%v_paths%;%ProgramData%\anaconda3
-set v_paths=%v_paths%;%USERPROFILE%\anaconda3
-
-for %%a in (%v_paths%) do (
-  IF NOT "%v_custom_path%"=="" (
-    set v_paths=%v_custom_path%;%v_paths%
-  )
-)
-
-for %%a in (%v_paths%) do (
-  if EXIST "%%a\Scripts\activate.bat" (
-    SET v_conda_path=%%a
-    echo anaconda3/miniconda3 detected in %%a
-    goto :CONDA_FOUND
-  )
-)
-
-IF "%v_conda_path%"=="" (
-  echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
-  pause
-  exit /b 1
-)
-
-:CONDA_FOUND
-echo Found Anaconda
-
-:SKIP_RESTORE
+:: Activate the base Conda environment
 call "%v_conda_path%\Scripts\activate.bat"
-echo %v_conda_env_name%
+if %errorlevel% neq 0 (
+    echo Failed to activate Conda. Error level: %errorlevel%
+    pause
+    exit /b 1
+)
 
+echo Conda activated successfully
+
+:: Create the conda environment
+echo Creating conda environment: %v_conda_env_name%
 call conda env create --name "%v_conda_env_name%" -f environment.yaml
+if %errorlevel% neq 0 (
+    echo Failed to create Conda environment. Error level: %errorlevel%
+    pause
+    exit /b 1
+)
 
+:: Activate the newly created environment
+echo Activating conda environment: %v_conda_env_name%
+call conda activate "%v_conda_env_name%"
+if %errorlevel% neq 0 (
+    echo Failed to activate Conda environment. Error level: %errorlevel%
+    pause
+    exit /b 1
+)
 
-
-call "%v_conda_path%\Scripts\activate.bat" "%v_conda_env_name%"
+echo Conda environment activated successfully
 
 :PROMPT
+echo Script completed successfully
 pause
